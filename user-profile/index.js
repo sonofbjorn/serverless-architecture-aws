13d48cc548e5998a8c8615487d3151c4c585d392
Leif Bjornson <labjornson@gmail.com>
2017-06-19 15:30:10 -0500

    Chapter 5: add custom-authorizer and remove jwt validation from user-profile function

--- user-profile/index.js
+++ user-profile/index.js
@@ -1,6 +1,5 @@
 "use strict"
 
-var jwt = require("jsonwebtoken")
 var request = require("request")
 
 exports.handler = function(event, context, callback) {
@@ -10,31 +9,22 @@ exports.handler = function(event, context, callback) {
   }
 
   var token = event.authToken.split(" ")[1]
+  var body = {
+    id_token: token
+  }
 
-  var secretBuffer = new Buffer(process.env.AUTH0_SECRET)
-  jwt.verify(token, secretBuffer, function(err, decoded) {
-    if (err) {
-      console.log("Failed jwt verification: ", err, "auth: ", event.authToken)
-      callback("Authorization Failed")
+  var options = {
+    url: "https://" + process.env.DOMAIN + "/tokeninfo",
+    method: "POST",
+    json: true,
+    body: body
+  }
+
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      callback(null, body)
     } else {
-      var body = {
-        id_token: token
-      }
-
-      var options = {
-        url: "https://" + process.env.DOMAIN + "/tokeninfo",
-        method: "POST",
-        json: true,
-        body: body
-      }
-
-      request(options, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          callback(null, body)
-        } else {
-          callback(error)
-        }
-      })
+      callback(error)
     }
   })
 }
